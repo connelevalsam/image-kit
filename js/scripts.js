4c3ebdd8135411344571aae60afa707bedc3a22a
ConnelBLAZE <connelblaze@gmail.com>
2017-09-25 16:53:33 +0100

    change nav

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+(function ($) {
+	"use strict";
+
+	// Page Loaded...
+	$(document).ready(function () {
+
+		/*==========  Responsive Navigation  ==========*/
+		$('.main-nav').children().clone().appendTo('.responsive-nav');
+		$('.responsive-menu-open').on('click', function(event) {
+			event.preventDefault();
+			$('body').addClass('no-scroll');
+			$('.responsive-menu').addClass('open');
+			return false;
+		});
+		$('.responsive-menu-close').on('click', function(event) {
+			event.preventDefault();
+			$('body').removeClass('no-scroll');
+			$('.responsive-menu').removeClass('open');
+			return false;
+		});
+    });
+
+})(jQuery);
